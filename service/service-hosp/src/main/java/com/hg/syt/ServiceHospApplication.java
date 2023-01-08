2fd35e2773f3f00b0f2d91ecc24df151343afe3c
HlkL <1961154734@qq.com>
2023-01-08 19:33:28 +0800

    医院服务

---
+++ service/service-hosp/src/main/java/com/hg/syt/ServiceHospApplication.java
@@ -0,0 +1,19 @@
+package com.hg.syt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hougen
+ * @program syt_parent
+ * @description 医院启动类
+ * @create 2023-01-08 19:14
+ */
+@SpringBootApplication
+public class ServiceHospApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run( ServiceHospApplication.class,args );
+    }
+}
+
+
