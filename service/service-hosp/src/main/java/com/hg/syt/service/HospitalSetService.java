74a6c76fe9f4a4c489908b91f3d3330bba0fe709
HlkL <1961154734@qq.com>
2023-01-09 03:26:31 +0800

    医院预约挂号系统CRUD

--- service/service-hosp/src/main/java/com/hg/syt/service/HospitalSetService.java
+++ service/service-hosp/src/main/java/com/hg/syt/service/HospitalSetService.java
@@ -1,10 +1,26 @@
 package com.hg.syt.service;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hg.common.result.Result;
 import com.hg.syt.model.hosp.HospitalSet;
+import com.hg.syt.vo.hosp.HospitalSetQueryVo;
 
 /**
  * @author HG
  */
 public interface HospitalSetService extends IService<HospitalSet> {
+    /**
+     * 分页查询医院设置
+     *
+     * @param current
+     * @param limit
+     * @param hospitalSetQueryVo
+     * @return
+     */
+    Page<HospitalSet> pageHospSet( long current, long limit, HospitalSetQueryVo hospitalSetQueryVo );
+
+    Result saveHospSet( HospitalSet hospitalSet );
+
+    Result sendSecretKey( Long id );
 }
--- service/service-hosp/src/main/java/com/hg/syt/service/impl/HospitalSetServiceImpl.java
+++ service/service-hosp/src/main/java/com/hg/syt/service/impl/HospitalSetServiceImpl.java
@@ -1,10 +1,17 @@
 package com.hg.syt.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hg.common.result.Result;
+import com.hg.common.utils.MD5;
 import com.hg.syt.mapper.HospitalSetMapper;
 import com.hg.syt.model.hosp.HospitalSet;
 import com.hg.syt.service.HospitalSetService;
+import com.hg.syt.vo.hosp.HospitalSetQueryVo;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 /**
  * @author hougen
@@ -16,6 +23,40 @@ import org.springframework.stereotype.Service;
 public class HospitalSetServiceImpl extends ServiceImpl<HospitalSetMapper, HospitalSet>
         implements HospitalSetService {
 
+    @Override
+    public Page<HospitalSet> pageHospSet( long current, long limit, HospitalSetQueryVo hospitalSetQueryVo ) {
+
+        //创建page对象
+        Page<HospitalSet> page = new Page<>( current, limit );
+        //条件构造器
+        QueryWrapper<HospitalSet> queryWrapper = new QueryWrapper<>();
+        queryWrapper.like( hospitalSetQueryVo.getHosname() != null, "hosname", hospitalSetQueryVo.getHosname() )
+                .eq( hospitalSetQueryVo.getHoscode() != null, "hoscode", hospitalSetQueryVo.getHoscode() );
+
+        return this.page( page, queryWrapper );
+    }
+
+    @Override
+    public Result saveHospSet( HospitalSet hospitalSet ) {
+
+        //设置状态 1 使用,0不能使用
+        hospitalSet.setStatus( 1 );
+        //签名密钥
+        Random random = new Random();
+        hospitalSet.setSignKey( MD5.encrypt( System.currentTimeMillis() + random.nextInt( 10000 ) + "" ) );
+        boolean save = this.save( hospitalSet );
+
+        return save ? Result.ok( "添加医院设置成功" ) : Result.fail( "添加失败" );
+    }
+
+    @Override
+    public Result sendSecretKey( Long id ) {
+
+        HospitalSet hospitalSet = this.getById( id );
+
+        //TODO 发送短信
+        return Result.ok( hospitalSet.getSignKey() );
+    }
 }
 
 
