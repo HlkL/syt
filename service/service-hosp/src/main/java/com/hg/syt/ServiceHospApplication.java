d273eb392247d7d020f7c5d3af3e08776ed67d7d
HlkL <1961154734@qq.com>
2023-01-13 15:26:56 +0800

    开启feign

--- service/service-hosp/src/main/java/com/hg/syt/ServiceHospApplication.java
+++ service/service-hosp/src/main/java/com/hg/syt/ServiceHospApplication.java
@@ -1,20 +1,10 @@
 package com.hg.syt;
 
-import org.apache.http.conn.util.DomainType;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.event.EventListener;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.index.IndexOperations;
-import org.springframework.data.mongodb.core.index.IndexResolver;
-import org.springframework.data.mongodb.core.index.MongoPersistentEntityIndexResolver;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
-
-import javax.annotation.Resource;
 
 /**
  * @author hougen
@@ -22,27 +12,16 @@ import javax.annotation.Resource;
  * @description 医院启动类
  * @create 2023-01-08 19:14
  */
-//@SpringBootApplication(exclude = MongoAutoConfiguration.class)
 @SpringBootApplication
 @MapperScan("com.hg.syt.mapper")
 @ComponentScan(basePackages = "com.hg")
+@EnableDiscoveryClient
 public class ServiceHospApplication {
 
-//    @Resource
-//    private MongoTemplate mongoTemplate;
-//
-//    @Resource
-//    private MongoMappingContext mongoMappingContext;
     public static void main( String[] args ) {
         SpringApplication.run( ServiceHospApplication.class,args );
     }
 
-//    @EventListener( ApplicationReadyEvent.class)
-//    public void initIndicesAfterStartup() {
-//        IndexOperations indexOps = mongoTemplate.indexOps(DomainType.class);
-//        IndexResolver resolver = new MongoPersistentEntityIndexResolver(mongoMappingContext);
-//        resolver.resolveIndexFor( DomainType.class).forEach(indexOps::ensureIndex);
-//    }
 }
 
 
