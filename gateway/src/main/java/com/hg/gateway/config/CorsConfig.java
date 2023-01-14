8a48138242dcfeb0d66d4e11fa810b0e688fed93
HlkL <1961154734@qq.com>
2023-01-15 00:40:22 +0800

    跨域配置

---
+++ gateway/src/main/java/com/hg/gateway/config/CorsConfig.java
@@ -0,0 +1,34 @@
+package com.hg.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.util.pattern.PathPatternParser;
+
+/**
+ * @author hougen
+ * @program syt_parent
+ * @description 跨域配置
+ * @create 2023-01-15 00:38
+ */
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedMethod("*");
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource(new PathPatternParser());
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
+    }
+
+}
+
+
