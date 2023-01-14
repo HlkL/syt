f68b041befdbafe92263626ef24f4e3a372c11e4
HlkL <1961154734@qq.com>
2023-01-14 23:44:01 +0800

    科室排班管理

--- service/service-hosp/src/main/java/com/hg/syt/service/ScheduleService.java
+++ service/service-hosp/src/main/java/com/hg/syt/service/ScheduleService.java
@@ -1,8 +1,11 @@
 package com.hg.syt.service;
 
+import com.hg.syt.model.hosp.Schedule;
 import org.springframework.data.domain.Page;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author hougen
@@ -29,6 +32,25 @@ public interface ScheduleService {
      * @param request request
      */
     void removeSchedule( HttpServletRequest request );
+
+    /**
+     * 根据医院编号 和 科室编号 查询排班规则数据分页显示
+     * @param page page
+     * @param limit limit
+     * @param hoscode 医院编号
+     * @param depcode 科室编号
+     * @return 排班规则数据
+     */
+    Map<String, Object> getRuleSchedule( Integer page, Integer limit, String hoscode, String depcode );
+
+    /**
+     * 根据医院编号 、科室编号和工作日期，查询排班详细信息
+     * @param hoscode 医院编号
+     * @param depcode 科室编号
+     * @param workDate 工作日期
+     * @return 排班详细信息
+     */
+    List<Schedule> getDetailSchedule( String hoscode, String depcode, String workDate );
 }
 
 
