470a6b24278e61c83d0ee27c6129033c972689ab
HlkL <1961154734@qq.com>
2023-01-15 00:23:25 +0800

    gateway网关服务配置

---
+++ gateway/src/main/java/com/hg/gateway/ServiceGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.hg.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author hougen
+ * @program syt_parent
+ * @description
+ * @create 2023-01-15 00:05
+ */
+@SpringBootApplication
+public class ServiceGatewayApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run( ServiceGatewayApplication.class,args );
+    }
+}
+
+
