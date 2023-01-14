6965a204678950420c343d351adc06d1a1f0e738
HlkL <1961154734@qq.com>
2023-01-14 23:43:21 +0800

    科室排班管理

---
+++ service/service-hosp/src/main/java/com/hg/syt/controller/ScheduleController.java
@@ -0,0 +1,50 @@
+package com.hg.syt.controller;
+
+import com.hg.common.result.Result;
+import com.hg.syt.model.hosp.Schedule;
+import com.hg.syt.service.ScheduleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author hougen
+ * @program syt_parent
+ * @description
+ * @create 2023-01-14 21:56
+ */
+@Api( tags = "科室排班管理")
+@RestController
+@RequestMapping("/admin/hosp/schedule")
+@CrossOrigin
+public class ScheduleController {
+
+    @Resource
+    private ScheduleService scheduleService;
+
+
+    @ApiOperation(value ="查询排班规则数据")
+    @GetMapping("getScheduleRule/{page}/{limit}/{hoscode}/{depcode}")
+    public Result getScheduleRule( @PathVariable Integer page,
+                                   @PathVariable Integer limit,
+                                   @PathVariable String hoscode,
+                                   @PathVariable String depcode) {
+        Map<String,Object> map = scheduleService.getRuleSchedule(page,limit,hoscode,depcode);
+        return Result.ok(map);
+    }
+
+    @ApiOperation(value = "查询排班详细信息")
+    @GetMapping("getScheduleDetail/{hoscode}/{depcode}/{workDate}")
+    public Result getScheduleDetail( @PathVariable String hoscode,
+                                     @PathVariable String depcode,
+                                     @PathVariable String workDate) {
+        List<Schedule> list = scheduleService.getDetailSchedule(hoscode,depcode,workDate);
+        return Result.ok(list);
+    }
+}
+
+
