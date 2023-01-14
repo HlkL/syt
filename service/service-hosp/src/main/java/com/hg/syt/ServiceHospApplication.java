efea342017508e4ec50ca20f3eb5fd3514f40106
HlkL <1961154734@qq.com>
2023-01-14 21:11:51 +0800

    开启feign

--- service/service-hosp/src/main/java/com/hg/syt/ServiceHospApplication.java
+++ service/service-hosp/src/main/java/com/hg/syt/ServiceHospApplication.java
@@ -1,10 +1,14 @@
 package com.hg.syt;
 
+import com.hg.feign.client.DictFeignClient;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+
+import javax.annotation.Resource;
 
 /**
  * @author hougen
@@ -16,6 +20,7 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan("com.hg.syt.mapper")
 @ComponentScan(basePackages = "com.hg")
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.hg.feign.client")
 public class ServiceHospApplication {
 
     public static void main( String[] args ) {
