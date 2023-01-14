aba33184aca8260ba7ba950151fdb67637bac826
HlkL <1961154734@qq.com>
2023-01-14 23:45:11 +0800

    findScheduleByHoscodeAndDepcodeAndWorkDate

--- service/service-hosp/src/main/java/com/hg/syt/repository/ScheduleRepository.java
+++ service/service-hosp/src/main/java/com/hg/syt/repository/ScheduleRepository.java
@@ -4,6 +4,9 @@ package com.hg.syt.repository;
 import com.hg.syt.model.hosp.Schedule;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * @author HG
  */
@@ -11,4 +14,6 @@ public interface ScheduleRepository extends MongoRepository<Schedule,String> {
     Schedule getScheduleByHoscode( String hoscode );
 
     Schedule getScheduleByHoscodeAndHosScheduleId( String hoscode, String hosScheduleId );
+
+    List<Schedule> findScheduleByHoscodeAndDepcodeAndWorkDate( String hoscode, String depcode, Date toDate );
 }
