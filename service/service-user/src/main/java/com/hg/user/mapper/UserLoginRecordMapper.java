3bcf4d6fe6e36476ad62647dd5913d1944f6e739
HlkL <1961154734@qq.com>
2023-01-18 01:33:08 +0800

    单点登录

---
+++ service/service-user/src/main/java/com/hg/user/mapper/UserLoginRecordMapper.java
@@ -0,0 +1,15 @@
+package com.hg.user.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hg.syt.model.user.UserLoginRecord;
+
+/**
+ * @author hougen
+ * @program syt_parent
+ * @description
+ * @create 2023-01-18 00:56
+ */
+public interface UserLoginRecordMapper extends BaseMapper<UserLoginRecord> {
+}
+
+
